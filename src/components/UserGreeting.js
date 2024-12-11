5cca8ab5ba22e811cde02074f8fc495e76e414e5
juniorbueno1988 <juniobueno@gmail.com>
2024-12-11 10:47:55 -0300

    responsivo do usergreeting

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import "./UserGreeting.css";
-import { FaCog } from "react-icons/fa";
+import { FaCog, FaBars } from "react-icons/fa"; // Importando o ícone do menu hambúrguer
 
 const UserGreeting = ({ userName }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado para controlar a abertura do menu
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Alterna o estado do menu
+  };
+
   return (
     <div className="user-greeting">
       <Helmet>
@@ -13,9 +19,26 @@ const UserGreeting = ({ userName }) => {
         />
       </Helmet>
       <span className="user-greeting-text">Olá, {userName || "Usuário"}!</span>
+
       <button className="profile-button">
         Configurar Perfil <FaCog className="icon" />
       </button>
+
+      {/* Menu Hambúrguer */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <FaBars className="hamburger-icon" />
+      </div>
+
+      {/* Menu Lateral */}
+      {isMenuOpen && (
+        <div className="side-menu">
+          <ul>
+            <li>Home</li>
+            <li>Perfil</li>
+            <li>Configurações</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
